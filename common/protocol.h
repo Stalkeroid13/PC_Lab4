a59f0fa1644d9f98e37d7da7ee609f5161bc83e3
Stalkeroid <zelinskyi.andrii@lll.kpi.ua>
2026-04-24 19:25:34 +0300

    server base

--- common/protocol.h
+++ common/protocol.h
@@ -1,4 +1,28 @@
 #ifndef PC_LAB4_PROTOCOL_H
 #define PC_LAB4_PROTOCOL_H
 
-#endif //PC_LAB4_PROTOCOL_H
+#include <winsock2.h>
+#include <cstdint>
+
+// Константи для команд
+enum class Command : uint32_t
+{
+    SET_CONFIG = 1,         // Надіслати розмір матриці та кількість потоків
+    SEND_DATA = 2,          // Надіслати саму матрицю
+    START_PROCESSING = 3,   // Команда на початок обчислень
+    GET_STATUS = 4,         // Запит статусу
+    GET_RESULT = 5          // Запит готової матриці
+};
+
+// Заголовок пакету
+#pragma pack(push, 1)       // Вимикаємо вирівнювання пам'яті для точної передачі по мережі
+struct PacketHeader
+{
+    uint32_t commandId;     // ID команди (з enum Command)
+    uint32_t payloadSize;   // Розмір даних, що йдуть ПІСЛЯ цього заголовка
+};
+#pragma pack(pop)
+
+constexpr int DEFAULT_PORT = 7500;
+
+#endif
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <thread>
+#include <vector>
+#include "protocol.h"
 
 using namespace std;
 
+void handleClient(SOCKET client_socket)
+{
+    cout << "Thread " << this_thread::get_id() << " handling new client...\n";
+}
+
 int main()
 {
+    // Ініціалізація WinSock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        cerr << "WSAStartup failed!\n";
+        return 1;
+    }
+
+    // Створення сокета для прослуховуванн
+    SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_socket == INVALID_SOCKET)
+    {
+        WSACleanup();
+        cerr << "Socket failed!\n";
+        return 1;
+    }
+
+    // Налаштування адреси (всі інтерфейси, порт 7500)
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(DEFAULT_PORT);
+
+    // Прив'язка та перехід у режим очікування
+    if (bind(listen_socket, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == SOCKET_ERROR)
+    {
+        closesocket(listen_socket);
+        WSACleanup();
+        cerr << "Bind failed!\n";
+        return 1;
+    }
+
+    listen(listen_socket, SOMAXCONN);
+    cout << "Server started on port " << DEFAULT_PORT << ". Waiting for clients...\n";
+
+    while (true)
+    {
+        SOCKET client_socket = accept(listen_socket, nullptr, nullptr);
+        if (client_socket == INVALID_SOCKET)
+            continue;
+
+        cout << "New client connected!\n";
+
+        // Створюємо новий потік для обслуговування клієнта
+        // Використовуємо detach, щоб потік працював незалежно від main
+
+        thread client_thread(handleClient, client_socket);
+        client_thread.detach();
+    }
+
+    closesocket(listen_socket);
+    WSACleanup();
     return 0;
 }
